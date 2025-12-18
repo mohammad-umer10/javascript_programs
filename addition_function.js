0becefadfaddfdd1c2605d8201010f5152240752
mohammad-umer10 <mohammadumerabbasi10@gmail.com>
2025-12-18 08:07:54 -0800

    Day_01

---
+++ addition_function.js
@@ -0,0 +1,26 @@
+// SIMPLE_ADDITION
+
+let x = 5;
+let y = 12;
+let z = x + y;
+console.log(z);
+
+let c = 5;
+let d = 18;
+let sum = c + d; 
+console.log(sum);
+
+let e, f, g;    
+e = 5;          
+f = 20;          
+g = e + f;      
+console.log(g);
+
+// ADDITION_FUNCTION
+
+function addition(a, b) {
+  return a + b;
+}
+
+let result = addition(5, 3);
+console.log("Addition ans is: " , result);
