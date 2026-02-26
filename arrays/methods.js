64d1c130151297aa302ae01f93934b1b01af501a
mohammad umer <mohammadumerabbasi10@gmail.com>
2026-02-26 17:06:21 +0500

    updated

--- arrays/methods.js
+++
@@ -1,7 +0,0 @@
-// const fruits = ["Banana", "Orange", "Apple", "Mango"];
-// let length = fruits.length;
-// console.log(length);
-
-const fruits = ["Banana", "Orange", "Apple", "Mango"];
-let fruit = fruits[fruits.length - 1];
-console.log(fruit);
