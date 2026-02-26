485a052891969d044c943d0a42485efb34d24012
mohammad umer <mohammadumerabbasi10@gmail.com>
2026-02-26 17:01:32 +0500

    completed pyramid task in function

---
+++ arrays/methods.js
@@ -0,0 +1,7 @@
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// let length = fruits.length;
+// console.log(length);
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let fruit = fruits[fruits.length - 1];
+console.log(fruit);
